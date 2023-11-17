fa89c114ad9a699a33a05dc173cf42795e23532c
Iaroslav <yarodegtyarev@gmail.com>
2023-11-17 20:43:45 +0500

    Initial Commit

---
+++ Rick&Morty/Rick_MortyApp.swift
@@ -0,0 +1,17 @@
+//
+//  Rick_MortyApp.swift
+//  Rick&Morty
+//
+//  Created by Ярослав Дегтярев on 17.11.2023.
+//
+
+import SwiftUI
+
+@main
+struct Rick_MortyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
